fe04284ec70582f1fb2be9c017e1a560426df8e6
jafarlow <jacobafarlow@gmail.com>
2019-10-29 16:01:47 -0400

    Initialized JS file with first variables
    events.js
    - Created empty variables for questions, questionsCount, and
    currentQuestion
    - Set variables for the HTML titles, answers, and action-button

---
+++ assets/scripts/events.js
@@ -0,0 +1,7 @@
+let questions
+let questionsCount
+let currentQuestion
+
+let question-title-element = document.getElementById("title")
+let answers-element = document.getElementById("answers")
+let action-button = document.getElementById("action-button")
