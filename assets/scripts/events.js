7f2beb4693a5d1e286f0a756c64c6f365cecbb4c
jafarlow <jacobafarlow@gmail.com>
2019-10-29 16:13:15 -0400

    Created getQuestions()
    events.js
    - created getQuestions()
    - tested with console.log(questions) to confirm that the request
    successfully pulls in the questions

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -2,6 +2,20 @@ let questions
 let questionsCount
 let currentQuestion
 
-let question-title-element = document.getElementById("title")
-let answers-element = document.getElementById("answers")
-let action-button = document.getElementById("action-button")
+let questionTitleElement = document.getElementById("title")
+let answersElement = document.getElementById("answers")
+let actionButton = document.getElementById("action-button")
+
+const getQuestions = function () {
+  let request = new XMLHttpRequest()
+  request.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      questions = JSON.parse(this.responseText).questions
+      console.log(questions);
+    }
+  }
+  request.open("GET", "./assets/questions.json", false)
+  request.send()
+}
+
+getQuestions()
