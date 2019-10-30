1bad47ba627fe9c5abde639d06a8a0594a647358
jafarlow <jacobafarlow@gmail.com>
2019-10-30 11:28:03 -0400

    Added button functionality
    events.js
    - Created score and nextButton variables at the top of the file
    - Created functionality for actionButton
      - Set check for correctness of user choice and increment score if
      needed
      - move to next question
      - show/hide necessary buttons
    - Created functionality for nextButton
      - Set end of quiz reqs
      - move to next question
      - show/hide necessary buttons
    
    NOTE: there's more work to do once a user gets to the end of the quiz
    - Final next button should read "See score"
    - There should be a reset quiz button on the view score screen
      - reset should put conditions back to the initial

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,10 +1,12 @@
 let questions
 let questionsCount
 let currentQuestion
+let score = 0
 
 let questionTitleElement = document.getElementById("title")
 let answersElement = document.getElementById("answers")
 let actionButton = document.getElementById("action-button")
+let nextButton = document.getElementById("next-button")
 
 const getQuestions = function () {
   let request = new XMLHttpRequest()
@@ -38,6 +40,7 @@ const displayQuestion = function (question) {
     answerInput.setAttribute("value", answer.id)
     answerInput.classList.add("answer")
 
+    // answer.answer accesses the JSON
     let answerTitle = document.createTextNode(answer.answer)
     label.appendChild(answerInput)
     label.appendChild(answerTitle)
@@ -45,6 +48,44 @@ const displayQuestion = function (question) {
     answersElement.appendChild(label)
   })
 }
+
+actionButton.addEventListener("click", function () {
+  let answers = document.getElementsByClassName("answer")
+
+  for (let i = 0; i < answers.length; i++) {
+    let answer = answers[i]
+    let question = questions[currentQuestion]
+    if (answer.checked && answer.value == question.correct) {
+      answer.parentNode.classList.add("correct")
+      score++
+    } else if (answer.checked && answer.value != question.correct) {
+      answer.parentNode.classList.add("incorrect")
+    }
+
+    // prevents more than one attempt at a question per session
+    answer.disabled = true
+  }
+
+  currentQuestion++
+  // shows the "Next Question" button
+  nextButton.classList.remove("hide")
+  // hides the "Submit" button
+  this.classList.add("hide")
+})
+
+nextButton.addEventListener("click", function () {
+  if (currentQuestion == questionsCount) {
+    document.getElementById("question-element").classList.add("hide")
+    document.getElementById("scores").classList.remove("hide")
+    document.getElementById("score").innerHTML = score + "/" + questionsCount
+    return
+  }
+  displayQuestion(questions[currentQuestion])
+  // shows the "Next Question" button
+  actionButton.classList.remove("hide")
+  // hides the "Submit" button
+  this.classList.add("hide")
+})
 
 // Initialization
 getQuestions()
